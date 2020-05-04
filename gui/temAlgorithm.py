6f2d668d05ae82ccec26496ad899875665c42531
bktl4ever <amangeldi99@gmail.com>
2020-05-04 14:11:44 +0600

    python dijkstra fix

--- gui/temAlgorithm.py
+++ gui/temAlgorithm.py
@@ -1,44 +1,37 @@
 
+from heapq import *
+
 # NOT FINISHED YET
 # Currently problem is in line  19
 def dijkstra(g,n,src,dest,dist,path):
-    q = [] # set<pair<int, int>> q; *******
     d = [-1] * n # vector<int> d(n, -1);
     p = [None] * n # vector<pair<int, int>> p(n);
     d[src] = 0
     p[src] = [src,0]
-    q.append([d[src], src])
+    q = [(0, src)]
     while (q):
-        v = q[0][1]
-        q.remove(q[0])
+        v = heappop(q)[1]
         for i in g[v]:
             u = i[0]
             w = i[1]
             if d[u] == -1 or d[u] > d[v] + w:
-                print (d[u],'/',u,' ')
-                q.remove([d[u],u])
                 d[u] = d[v] + w
                 p[u] = [v,w]
-                q.append[d[u],u]
-
-                tem = list(set(q))
-                tem.sort(key=q.index)
-                q = tem
+                heappush(q, (d[u], u))
             
     dist = d[dest]
     if dist != -1:
         v = dest
         while v != src:
-            path.append[ [p[v][0],v], p[v][1] ]
+            path.append([[p[v][0],v], p[v][1]])
             v = p[v][0]
         path.reverse()
     return
     
 
 n = 5
-g = []
-for i in range(0,n):
-    g.append([])
+g = [[] for _ in range (0, n)]
+
 g[0].append([1,2])
 g[1].append([0,2])
 
@@ -64,9 +57,9 @@ src,dest,dist = 3, 0, 0
 path = []
 dijkstra(g,n,src,dest,dist,path)
 if dist == -1:
-    print('there is no any path from %d to %d\n' %(src + 1, dest + 1))
+    print('there is no any path from %d to %d' %(src + 1, dest + 1))
 else:
-    print('shortest path from %d to %d has a total weight %d and consists of %d edges.\n' %(src + 1, dest + 1, dist, len(path)) )
+    print('shortest path from %d to %d has a total weight %d and consists of %d edges:\n' %(src + 1, dest + 1, dist, len(path)) )
     for i in path :
-        printt('%d %d %d\n'%(i[0][0] + 1, i[0][1] + 1, i[1]))
+        print('%d %d %d'%(i[0][0] + 1, i[0][1] + 1, i[1]))
 
